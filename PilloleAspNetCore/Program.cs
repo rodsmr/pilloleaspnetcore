187967b4dc0a1435a8a41a04400db63bb7d96fed
Marco Rodella <m.rodella@invisiblefarm.com>
2025-05-20 23:17:07 +0200

    ✨ Singleton service

--- PilloleAspNetCore/Program.cs
+++ PilloleAspNetCore/Program.cs
@@ -1,9 +1,36 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Servizi per funzionare
+// Servizi
+
+// Singleton: instanza sarà sempre la stessa per tutta la vita dell'applicazione
+builder.Services.AddSingleton<MyService>(); // questa riga mi evita di creare un oggetto MyService ogni volta che viene chiamato il servizio
+// Scoped
+// Transient
 var app = builder.Build();
 
 // Middleware
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (MyService myService) => 
+{
+    // var myService = new MyService(); // riga rimossa dopo aver registrato il servizio
+    // il servizio ora viene iniettato
+    return $"Hello World! {myService.GetValue()}";
+});
 
 app.Run();
+
+#region Service
+public class MyService
+{
+    private static int value = 0;
+
+    public MyService()
+    {
+        value++;
+    }
+
+    public int GetValue()
+    {
+        return value;
+    }
+}
+#endregion
